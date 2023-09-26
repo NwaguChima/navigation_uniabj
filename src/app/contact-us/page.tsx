51c54d57f4e96975f51fad6347fd26df5e24bdfa
NwaguChima <nwaguchima48@gmail.com>
2023-09-26 04:58:43 +0100

    setup pages

---
+++ src/app/contact-us/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Page = ({}) => {
+  return (
+    <section>
+      <h1 className="text-2xl md:text-3xl font-bold">Contact Us</h1>
+    </section>
+  );
+};
+
+export default Page;
---
+++ src/app/map/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Page = ({}) => {
+  return (
+    <section>
+      <h1 className="text-2xl md:text-3xl font-bold">Map</h1>
+    </section>
+  );
+};
+
+export default Page;
