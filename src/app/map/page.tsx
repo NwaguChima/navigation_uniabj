24e218ddeb03633c666fee132d13ab67574fe779
NwaguChima <nwaguchima48@gmail.com>
2023-10-01 23:29:30 +0100

    added map and spinner

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -1,9 +1,14 @@
+import Map from "@/components/Map";
 import React from "react";
 
 const Page = ({}) => {
   return (
     <section className="p-2 pt-12 md:p-6 md:pt-12">
       <h1 className="text-2xl font-bold md:text-3xl">Map</h1>
+
+      <div>
+        <Map />
+      </div>
     </section>
   );
 };
---
+++ src/components/Map.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import React from "react";
+import { useMemo } from "react";
+import {
+  GoogleMap,
+  useLoadScript,
+  Marker,
+  InfoWindow,
+} from "@react-google-maps/api";
+import { ThreeCircles } from "react-loader-spinner";
+
+interface MapProps {}
+
+const Map: React.FC<MapProps> = ({}) => {
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY!,
+  });
+
+  const center = useMemo(() => ({ lat: 8.9807, lng: 7.1805 }), []);
+
+  if (!isLoaded)
+    return (
+      <div className="flex h-[70vh] items-center justify-center">
+        <ThreeCircles
+          height="100"
+          width="100"
+          color="#4fa94d"
+          wrapperStyle={{}}
+          wrapperClass=""
+          visible={true}
+          ariaLabel="three-circles-rotating"
+          outerCircleColor=""
+          innerCircleColor=""
+          middleCircleColor=""
+        />
+      </div>
+    );
+  if (loadError) return <div>Error loading maps</div>;
+
+  return (
+    <div className="mt-6 h-[70vh] overflow-hidden rounded-sm border bg-zinc-100">
+      <GoogleMap
+        mapContainerStyle={{ width: "100%", height: "100%" }}
+        zoom={10}
+        center={center}
+      >
+        <Marker position={center} />
+      </GoogleMap>
+    </div>
+  );
+};
+
+export default Map;
