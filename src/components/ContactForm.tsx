bd14111c7353491474e01f0688841657367599bc
NwaguChima <nwaguchima48@gmail.com>
2023-10-24 23:49:20 +0100

    updated form functionality

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { FormEvent, useRef, useState } from "react";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { ContactRequest, ContactValidator } from "@/lib/validators/contact";
@@ -6,10 +7,16 @@ import { Label } from "./ui/Label";
 import { Input } from "./ui/Input";
 import { Button } from "./ui/Button";
 import { Textarea } from "./ui/Textarea";
+import emailjs from "@emailjs/browser";
+import { useToast } from "@/hooks/use-toast";
 
 interface ContactFormProps {}
 
 const ContactForm: React.FC<ContactFormProps> = ({}) => {
+  const formRef = useRef<HTMLFormElement>(null);
+  const [loading, setLoading] = useState(false);
+  const { toast } = useToast();
+
   const {
     handleSubmit,
     register,
@@ -23,10 +30,45 @@ const ContactForm: React.FC<ContactFormProps> = ({}) => {
     },
   });
 
+  const sendEmail = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setLoading(true);
+
+    emailjs
+      .sendForm(
+        process.env.NEXT_PUBLIC_SERVICE_ID!, // SERVICE_ID
+        process.env.NEXT_PUBLIC_TEMPLATE_ID!, // TEMPLATE_ID
+        formRef.current!,
+        process.env.NEXT_PUBLIC_PUBLIC_KEY!, // PUBLIC KEY
+      )
+      .then(
+        (result: any) => {
+          toast({
+            title: "Email Sent to Admin",
+            description: "Your message has been sent successfully.",
+          });
+          setLoading(false);
+          formRef.current!.reset();
+        },
+        ({ text }: { text: string }) => {
+          toast({
+            title: "Email Not Sent",
+            description: `${text}`,
+            variant: "destructive",
+          });
+          setLoading(false);
+        },
+      );
+  };
+
   return (
     <div className="formGradient mb-28 mt-10 max-w-xl rounded-lg border p-5">
       <form
-        onSubmit={handleSubmit((data) => console.log(data))}
+        ref={formRef}
+        name="uniAbuja_contact_form"
+        onSubmit={(e) => {
+          handleSubmit((data) => sendEmail(e))(e);
+        }}
         className="flex flex-col gap-4"
       >
         <div className="flex flex-col">
@@ -73,7 +115,11 @@ const ContactForm: React.FC<ContactFormProps> = ({}) => {
           )}
         </div>
 
-        <Button className="mt-4 bg-green-500 transition-all duration-300 hover:bg-green-600">
+        <Button
+          isLoading={loading}
+          type="submit"
+          className="mt-4 bg-green-500 transition-all duration-300 hover:bg-green-600"
+        >
           Submit
         </Button>
       </form>
